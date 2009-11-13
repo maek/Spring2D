7704c0da08a75112d83eea21ee75e44982f28de2
maek <maek@paranoici.org>
2009-11-13 14:20:52 +0100

    Added the s2Vector.* files & the src/ directory
    - Created a first uncompleted class for the 2D vectors
    - Added the ==, != and << operators

---
+++ include/s2Vector.h
@@ -0,0 +1,62 @@
+#ifndef __VECTOR_H__
+#define __VECTOR_H__
+
+#include "s2Common.h"
+
+
+namespace Spring2D
+{
+
+  // ---------------------------------------------------------------------------
+  // Class for the 2D vectors
+  class Vector
+  {
+    public:
+
+      Real x;
+      Real y;
+
+
+    public:
+
+      // Constructor
+      explicit Vector (const Real X = 0.0, const Real Y = 0.0) : x(X), y(Y) { }
+
+      // Assignment
+      Vector& operator= (const Vector& V)
+      {
+        x = V.x;
+        y = V.y;
+
+        return *this;
+      }
+
+  };
+
+
+  // ---------------------------------------------------------------------------
+  // Equal
+  inline bool operator== (const Vector& V1, const Vector& V2)
+  {
+    return (V1.x == V2.x && V1.y == V2.y); 
+  }
+
+  // ---------------------------------------------------------------------------
+  // Not Equal
+  inline bool operator!= (const Vector& V1, const Vector& V2)
+  {
+    return (V1.x != V2.x || V1.y != V2.y); 
+  }
+
+  // ---------------------------------------------------------------------------
+  // Print
+  inline std::ostream& operator<< (std::ostream& os, const Vector& V)
+  {
+    return (os << "[" << V.x << ", " << V.y << "]");
+  }
+
+
+}
+
+
+#endif // __VECTOR_H__
---
+++ src/s2Vector.cc
@@ -0,0 +1,1 @@
+#include "../include/s2Vector.h"
