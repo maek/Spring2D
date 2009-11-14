46a3811c9c1c6b0fdc1887381dcaedfcab60679c
maek <maek@paranoici.org>
2009-11-14 20:10:00 +0100

    Created a unique header file for the applications
    - Added the existing headers to Spring2D.h

---
+++ include/Spring2D.h
@@ -0,0 +1,10 @@
+#ifndef __SPRING2D_H__
+#define __SPRING2D_H__
+
+// Include all header files of the library (for the applications)
+#include "s2Settings.h"
+#include "s2Engine.h"
+#include "s2Vector.h"
+
+
+#endif // __SPRING2D_H__
