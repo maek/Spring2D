f201e402f5856b606ff119083eb48647ce3ccd07
maek <maek@paranoici.org>
2009-11-19 21:23:29 +0100

    Fixed the Body class
    - Removed get & set functions
    - Fixed position, velocity & acceleration permissions

--- include/s2Body.h
+++ include/s2Body.h
@@ -13,59 +13,18 @@ namespace Spring2D
   {
     public:
 
+      Vector position;
+      Vector velocity;
+      Vector acceleration;
+
+
+    public:
+
       // Constructor
       Body (const Vector& POSITION = Vector::ZERO,
           const Vector& VELOCITY = Vector::ZERO)
-        : position_(POSITION), velocity_(VELOCITY), acceleration_(Vector::ZERO)
+        : position(POSITION), velocity(VELOCITY), acceleration(Vector::ZERO)
       { }
-
-
-      // Set the body position
-      void setPosition (const Vector& POSITION)
-      {
-        position_ = POSITION;
-      }
-      void setPosition (Real X, Real Y)
-      {
-        position_.x = X;
-        position_.y = Y;
-      }
-
-      // Set the body velocity
-      void setVelocity (const Vector& VELOCITY)
-      {
-        velocity_ = VELOCITY;
-      }
-      void setVelocity (Real X, Real Y)
-      {
-        velocity_.x = X;
-        velocity_.y = Y;
-      }
-
-      // Get the body position
-      Vector getPosition () const
-      {
-        return position_;
-      }
-
-      // Get the body velocity
-      Vector getVelocity () const
-      {
-        return velocity_;
-      }
-
-      // Get the body acceleration
-      Vector getAcceleration () const
-      {
-        return acceleration_;
-      }
-
-
-    private:
-
-      Vector position_;
-      Vector velocity_;
-      Vector acceleration_;
 
   };
 
