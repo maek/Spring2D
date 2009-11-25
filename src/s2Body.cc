e64e02a4865521649008d3a683a5442e56f321f9
maek <maek@paranoici.org>
2009-11-25 14:28:08 +0100

    Reverted to Euler
    - Left Verlet due to the acceleration step problem

--- src/s2Body.cc
+++ src/s2Body.cc
@@ -5,24 +5,20 @@ namespace Spring2D
 {
   // ---------------------------------------------------------------------------
   // Compute the new position & update velocity and acceleration
-  // (VELOCIY VERLET)
+  // (EULER)
   void Body::integrate (const Real& TIME_STEP)
   {
     if (static_)
       return;
 
     // (1) Update the position
-    position_ += (velocity_ * TIME_STEP) +
-      (0.5 * acceleration_ * TIME_STEP * TIME_STEP);
+    position_ += (velocity_ * TIME_STEP);
 
-    // (2) Update the velocity I
-    velocity_ += 0.5 * acceleration_ * TIME_STEP;
-
-    // (3) Update the acceleration
+    // (2) Update the acceleration
     acceleration_ = netForce_ * (1.0 / mass_);
 
-    // (4) Update the velocity II
-    velocity_ += 0.5 * acceleration_ * TIME_STEP;
+    // (3) Update the velocity
+    velocity_ += acceleration_ * TIME_STEP;
 
 
     // Clear the net force
